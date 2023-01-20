812ee7b728a9aae99c57a9afe5d2c5f1952d1fcf
MRFIRE-GOD <ttesfayohans22@gmail.com>
2023-01-20 09:20:48 +0100

    v.4

--- sql oppgave 3.py
+++ sql oppgave 3.py
@@ -1,12 +1,64 @@
-import sqlite3
+import sqlite3 
 import csv
-import os
 
-global cur
-global conn 
+con = sq.connect('info-info.db')
 
-def tabel(): 
-    global conn
-    conn = sqlite3.connect ("nummer.db")
+c.execute(
+    """
+    CREATE TABLE sales (
+        SalesID INTEGER ,
+        OrderID TEXT NOT NULL,
+        ProductID TEXT NOT NULL,
+        Sales REAL,
+        Quantity INTEGER,
+        Discount REAL,
+        Profit REAL,
+        PRIMARY KEY(SalesID),
+        FOREIGN KEY(OrderID) REFERENCES orders(OrderID),
+        FOREIGN KEY(ProductID) REFERENCES products(ProductID)
+        );
+     """
+)
 
-    
+# ORDERS
+c.execute(
+    """
+    CREATE TABLE orders (
+        OrderID TEXT,
+        OrderDate TEXT,
+        ShipDate TEXT,
+        ShipMode TEXT,
+        CustomerID TEXT,
+        PRIMARY KEY(OrderID),
+        FOREIGN KEY(CustomerID) REFERENCES customers(CustomerID)
+        );
+    """
+)
+
+# CUSTOMERS
+c.execute(
+    """
+    CREATE TABLE customers (
+        CustomerID TEXT PRIMARY KEY,
+        CustomerName TEXT,
+        Segment TEXT,
+        Country TEXT,
+        City TEXT,
+        State TEXT,
+        PostalCode TEXT,
+        Region TEXT
+        );
+    """
+)
+
+# PRODUCTS
+c.execute(
+    """
+    CREATE TABLE products (
+        ProductID TEXT PRIMARY KEY,
+        Category TEXT,
+        SubCategory TEXT,
+        ProductName TEXT
+        );
+    """
+)
