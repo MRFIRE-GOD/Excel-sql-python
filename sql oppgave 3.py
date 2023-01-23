9f223fa9942b106f3b5984c29ba22fe332ea449c
MRFIRE-GOD <ttesfayohans22@gmail.com>
2023-01-23 08:52:37 +0100

    v.5

--- sql oppgave 3.py
+++ sql oppgave 3.py
@@ -1,12 +1,26 @@
 import sqlite3
-import csv
-import os
+# Get customer number from user
+customer_number = input("Enter customer number: ")
 
-global cur
-global conn 
+# Connect to the database
+conn = sqlite3.connect('customer_list.db')
+c = conn.cursor()
 
-def tabel(): 
-    global conn
-    conn = sqlite3.connect ("nummer.db")
 
-    
+# Retrieve all information about the customer
+c.execute("SELECT * FROM customer_info WHERE customer_number = ?", (customer_number,))
+customer_info = c.fetchall()
+
+c.execute("SELECT * FROM postcode_table WHERE customer_number = ?", (customer_number,))
+postcode_info = c.fetchall()
+
+# Print the customer information
+print("Customer Number: ", customer_info[0])
+print("Name: ", customer_info[1])
+print("Address: ", customer_info[2])
+print("Postcode: ", customer_info[3])
+print("City: ", postcode_info[0])
+print("State: ", postcode_info[1])
+
+# Close the connection
+conn.close()
--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-import subprocess
+import pandas as pd
+import sqlite3
 
-def view_data_in_info_db():
-    subprocess.call(["sqlite3", "info.db", ".headers on", ".mode column", "SELECT * FROM name;"])
+# Connect to or create the database
+conn = sqlite3.connect('customer_list.db')
 
-view_data_in_info_db()
+# Read data from the first Excel file
+customer_info_df = pd.read_excel('Postnummerregister.csv')
+
+# Insert the data into the customer_info table
+customer_info_df.to_sql('customer_info', conn, if_exists='replace', index=False)
+
+# Read data from the second Excel file
+postcode_table_df = pd.read_excel('randoms.csv')
+
+# Insert the data into the postcode_table table
+postcode_table_df.to_sql('postcode_table', conn, if_exists='replace', index=False)
+
+# Close the connection
+conn.close()
