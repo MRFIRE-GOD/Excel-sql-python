aa4ca321bba2c919ce13e421f8e2e7bea2b52b8d
Thomas Tesfayohans <ttesfayohans22@gmail.com>
2023-01-26 08:38:16 +0100

    main

--- sql oppgave 3.py
+++ sql oppgave 3.py
@@ -1,64 +1,47 @@
-import sqlite3 
-import csv
+import sqlite3
 
-con = sq.connect('info-info.db')
+# Opprett forbindelse til databasen
+conn = sqlite3.connect("kundeliste.db")
+cursor = conn.cursor()
 
-c.execute(
-    """
-    CREATE TABLE sales (
-        SalesID INTEGER ,
-        OrderID TEXT NOT NULL,
-        ProductID TEXT NOT NULL,
-        Sales REAL,
-        Quantity INTEGER,
-        Discount REAL,
-        Profit REAL,
-        PRIMARY KEY(SalesID),
-        FOREIGN KEY(OrderID) REFERENCES orders(OrderID),
-        FOREIGN KEY(ProductID) REFERENCES products(ProductID)
-        );
-     """
-)
+# Opprett tabellene
+cursor.execute("CREATE TABLE kundeliste (kundenr INTEGER PRIMARY KEY, navn TEXT, adresse TEXT)")
+cursor.execute("CREATE TABLE kundeinfo (kundenr INTEGER PRIMARY KEY, telefonnr TEXT, epost TEXT)")
+cursor.execute("CREATE TABLE postnummer_tabell (postnr INTEGER PRIMARY KEY, poststed TEXT)")
 
-# ORDERS
-c.execute(
-    """
-    CREATE TABLE orders (
-        OrderID TEXT,
-        OrderDate TEXT,
-        ShipDate TEXT,
-        ShipMode TEXT,
-        CustomerID TEXT,
-        PRIMARY KEY(OrderID),
-        FOREIGN KEY(CustomerID) REFERENCES customers(CustomerID)
-        );
-    """
-)
+# Les dataene fra filen
+with open("data.txt", "r") as file:
+    data = file.readlines()
+    for line in data:
+        line = line.strip().split(",")
+        kundenr = line[0]
+        navn = line[1]
+        adresse = line[2]
+        telefonnr = line[3]
+        epost = line[4]
+        postnr = line[5]
+        poststed = line[6]
+        
+        # Legg til dataene i tabellene
+        cursor.execute("INSERT INTO kundeliste VALUES (?,?,?)", (kundenr, navn, adresse))
+        cursor.execute("INSERT INTO kundeinfo VALUES (?,?,?)", (kundenr, telefonnr, epost))
+        cursor.execute("INSERT INTO postnummer_tabell VALUES (?,?)", (postnr, poststed))
 
-# CUSTOMERS
-c.execute(
-    """
-    CREATE TABLE customers (
-        CustomerID TEXT PRIMARY KEY,
-        CustomerName TEXT,
-        Segment TEXT,
-        Country TEXT,
-        City TEXT,
-        State TEXT,
-        PostalCode TEXT,
-        Region TEXT
-        );
-    """
-)
+# Spør brukeren om kundenummer
+kundenr = input("Skriv inn kundenummer: ")
 
-# PRODUCTS
-c.execute(
-    """
-    CREATE TABLE products (
-        ProductID TEXT PRIMARY KEY,
-        Category TEXT,
-        SubCategory TEXT,
-        ProductName TEXT
-        );
-    """
-)
+# Hent dataene om kunden fra tabellene
+cursor.execute("SELECT * FROM kundeliste WHERE kundenr=?", (kundenr,))
+kunde_data = cursor.fetchone()
+cursor.execute("SELECT * FROM kundeinfo WHERE kundenr=?", (kundenr,))
+kundeinfo_data = cursor.fetchone()
+cursor.execute("SELECT poststed FROM postnummer_tabell WHERE postnr=?", (kunde_data[2],))
+poststed_data = cursor.fetchone()
+
+# Vis dataene om kunden
+print("Kundenummer: ", kundenr)
+print("Navn: ", kunde_data[1])
+print("Adresse: ", kunde_data[2])
+print("Telefonnr: ", kundeinfo_data[1])
+print("E-post: ", kundeinfo_data[2])
+print("Postnummer: ", kunde_data[2])
